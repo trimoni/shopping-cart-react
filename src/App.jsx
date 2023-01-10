f18ed9c7fce35a154f14d510e30d35ded09a03b5
Tal <trimoni8@gmail.com>
2023-01-10 13:54:07 -0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-  return <div className='App'></div>
+  return <div className='App'>
+
+    <Router>
+      <NavBar>
+
+      </NavBar>
+      <Routes>
+        <Route path='/' />
+        <Route path='/cart' />
+      </Routes>
+    </Router>
+  </div>
 }
 
 export default App
