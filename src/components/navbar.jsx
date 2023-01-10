f39be5a6fa0666b2390fabaed08fdca247899322
Tal <trimoni8@gmail.com>
2023-01-10 14:05:25 -0500

    add phosphur

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { ShoppingCart } from 'phosphor-react'
 
 export const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='links'>
         <Link to='/'> Shop </Link>
-        <Link></Link>
+        <Link to='/cart'> <ShoppingCart size={32}/> </Link>
       </div>
     </div>
   )
