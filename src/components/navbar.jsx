8263f6390ea2629b6d74c850912fcedfef10b546
Tal <trimoni8@gmail.com>
2023-01-10 14:02:44 -0500

    link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='links'>
+        <Link to='/'> Shop </Link>
+        <Link></Link>
+      </div>
+    </div>
   )
 }
