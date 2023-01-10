fd26489aa16cc39ceeb6053b1f1db3d22d964809
Tal <trimoni8@gmail.com>
2023-01-10 14:21:22 -0500

    import pics

--- src/products.js
+++ src/products.js
@@ -1,3 +1,25 @@
+import darkness from './assets/darkness.jpeg'
+import double from './assets/double.jpeg'
+import fairy from './assets/fairy.jpeg'
+import fighting from './assets/fighting.jpeg'
+import fire from './assets/fire.jpeg'
+import grass from './assets/grass.jpeg'
+import lightning from './assets/lightning.jpeg'
+import metal from './assets/metal.jpeg'
+import psychic from './assets/psychic.jpeg'
+import water from './assets/water.jpeg'
+
 export const PRODUCTS = [
-  
+  {
+    id: 1,
+    productName: "Dark Energy",
+    price: 999.0,
+    productImage: darkness,
+  },
+  {
+    id: 2,
+    productName: "Normal Energy",
+    price: 999.0,
+    productImage: darkness,
+  }
 ]
