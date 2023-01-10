6f0999e9d0eb0e541966676f788151433ab35b3d
Tal <trimoni8@gmail.com>
2023-01-10 13:58:40 -0500

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Navbar} from './components/navbar'
 
 function App() {
   return <div className='App'>
 
     <Router>
-      <NavBar>
-
-      </NavBar>
+      <Navbar />
       <Routes>
         <Route path='/' />
         <Route path='/cart' />
