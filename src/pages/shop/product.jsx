bfe8fafb75a3294d6d0a4b5d8db1a849fd2605df
Tal <trimoni8@gmail.com>
2023-01-10 15:38:07 -0500

    shop css

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -4,7 +4,7 @@ export const Product = (props) => {
   const {id, productName, price, productImage} = props.data
   return (
     <div className='product'>
-      <img src={productImage}/>
+      <img src={productImage} alt=''/>
       <div className='description'>
         <p> 
           <b>{productName}</b>
