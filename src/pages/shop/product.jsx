8b94dc3b9e8a4af6672960ab8523eb8601378600
Tal <trimoni8@gmail.com>
2023-01-11 09:26:34 -0500

    increase shopping list

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -3,7 +3,9 @@ import { ShopContext } from '../../context/shop-context'
 
 export const Product = (props) => {
   const {id, productName, price, productImage} = props.data
-    const {addToCart} = useContext(ShopContext)
+    const {addToCart, cartItems} = useContext(ShopContext)
+  
+    const cartItemAmount = cartItems[id]
   return (
     <div className='product'>
       <img src={productImage} alt=''/>
@@ -13,7 +15,8 @@ export const Product = (props) => {
           </p>
           <p> ${price}</p>
       </div>
-      <button className='addToCartBttn' onClick={() => addToCart(id)}> Add to Cart</button>
+      <button className='addToCartBttn' onClick={() => addToCart(id)}> 
+      Add to Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}</button>
       </div>
   )
 }
