9726a714862e3bb47a6f580fd0aa3648e7d9ebab
Tal <trimoni8@gmail.com>
2023-01-11 09:15:54 -0500

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Navbar} from './components/navbar'
 import { Shop } from './pages/shop/shop'
 import { Cart } from './pages/cart/cart'
+import { ShopContextProvider } from './context/shop-context'
 
 function App() {
   return <div className='App'>
-
+    <ShopContextProvider>
     <Router>
       <Navbar />
       <Routes>
@@ -14,6 +15,7 @@ function App() {
         <Route path='/cart' element={<Cart />}/>
       </Routes>
     </Router>
+    </ShopContextProvider>
   </div>
 }
 
