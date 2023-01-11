49ccbbc7c864feaf9984ca5928b7670abc69cc21
Tal <trimoni8@gmail.com>
2023-01-11 09:14:29 -0500

    import shopcontext

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -21,7 +21,9 @@ export const ShopContextProvider = (props) => {
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({...prev, [itemId]:prev[itemId] - 1}))
   }
+
+  const contextValue = {cartItems, addToCart, removeFromCart}
   return (
-    <ShopContext.Provider>{props.children}</ShopContext.Provider>
+    <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
   )
 }
