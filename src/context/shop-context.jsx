ecf38594bef6ed95e9ee6b138b16f3d8f4c9374c
Tal <trimoni8@gmail.com>
2023-01-11 09:12:53 -0500

    add and remove cart

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,9 +1,26 @@
 import React, { createContext, useState } from 'react'
+import { PRODUCTS } from '../products'
 
 export const ShopContext = createContext(null)
 
+const getDefaultCart = () => {
+  let cart = {}
+  for (let i = 1; i < PRODUCTS.length + 1; i++){
+    cart[i] = 0
+  }
+  return cart
+}
+
 export const ShopContextProvider = (props) => {
-  const [cartItems, setCartItems] = useState()
+  const [cartItems, setCartItems] = useState(getDefaultCart())
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({...prev, [itemId]:prev[itemId] + 1}))
+  }
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({...prev, [itemId]:prev[itemId] - 1}))
+  }
   return (
     <ShopContext.Provider>{props.children}</ShopContext.Provider>
   )
