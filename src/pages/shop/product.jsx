f65260f8a48ea3b10c1c6b93a120c4be41eb196d
Tal <trimoni8@gmail.com>
2023-01-11 09:19:08 -0500

    console log

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ShopContext } from '../../context/shop-context'
 
 export const Product = (props) => {
   const {id, productName, price, productImage} = props.data
+    const {addToCart} = useContext(ShopContext)
   return (
     <div className='product'>
       <img src={productImage} alt=''/>
@@ -12,7 +13,7 @@ export const Product = (props) => {
           </p>
           <p> ${price}</p>
       </div>
-      <button className='addToCartBttn'> Add to Cart</button>
+      <button className='addToCartBttn' onClick={() => addToCart(id)}> Add to Cart</button>
       </div>
   )
 }
