9fe130d601a13f99e2a0e97f85ade52189f1001d
Tal <trimoni8@gmail.com>
2023-01-11 09:37:45 -0500

    price

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-export const CartItem = () => {
+export const CartItem = (props)  => {
+  const {id, productName, price, productImage} = props.data
   return (
-    <div className='cartItem'>cart-item</div>  
+    
+    <div className='cartItem'>cart-item
+    
+    <img src={productImage} alt="" />
+    <div className='description'>
+      <p> <b> {productName}</b></p>
+      <p> ${price}</p>
+    </div>
+    </div>  
   )
 }
 
